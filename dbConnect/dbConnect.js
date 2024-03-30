21210745e1e44c09f9274e886e51e0aa92df24fc
PatvakanGevorgyan <gevorgyanpatvakan@gmail.com>
2024-03-30 13:26:14 +0400

    change somthing

--- dbConnect/dbConnect.js
+++ dbConnect/dbConnect.js
@@ -4,7 +4,7 @@ const url = "mongodb://127.0.0.1:27017"
 const dbConnect = async () => {
     try {
         const connect = await mongoose.connect(url)
-        console.log("Connected!")
+        console.log("Connected")
     } catch (error) {
         console.log(error);
         console.log("Error");
