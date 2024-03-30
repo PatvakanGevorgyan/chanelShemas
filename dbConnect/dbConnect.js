ed06520cd8f99ca9c2317b18ea2a3aac728137d8
PatvakanGevorgyan <gevorgyanpatvakan@gmail.com>
2024-03-30 12:58:43 +0400

    first commit

---
+++ dbConnect/dbConnect.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const url = "mongodb://127.0.0.1:27017"
+const dbConnect = async () => {
+    try {
+        const connect = await mongoose.connect(url)
+        console.log("Connected")
+    } catch (error) {
+        console.log(error);
+        console.log("Error");
+    }
+};
+
+export default dbConnect
